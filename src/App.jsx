7570504c42c04ab0c50924add7939c9b31807c1c
andresfdel17 <andresfdel13@hotmail.com>
2022-05-25 16:53:02 -0500

    Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
 import "./assets/css/App.css";
-import Navbar from  "./components/Navbar";
+import Navbar from "./components/Navbar";
 
 import "bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
+function App(props) {
+  const loadweb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.requestAccounts;
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert("No ethereum browser detected, Check Metamask");
+    }
+  }
+  useEffect(() => {
+     loadweb3();
+  }, []);
+  const [account, setAccount] = useState("0x0");
   return (
     <>
-      <Navbar />
+      <Navbar account={account} />
       <div className="text-center">
         Hello world
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ function Navbar (props){
 
     return ( 
         <nav className="navbar navbar-dark flex-top shadow p-0" style={{backgroundColor: "#000000", height: "50px"}}>
-            <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#" style={{color: "#ffffff"}}>DAPP Yield Stakin &nbps;
+            <a href="/" className="navbar-brand col-sm-3 col-md-2 mr-0" style={{color: "#ffffff"}}>DAPP Yield Stakin &nbsp; 
                 <img src={img} width="50" height="30" className="d-inline-block align-top" alt="bank" />
             </a>
             <ul className="navbar-nav px-3">
                 <li className="text-nowrap d-none nav-item d-sm-block">
-                    <small style={{color: "#ffffff"}}> Account Number</small>
+                    <small style={{color: "#ffffff"}}> Account Number: {props.account}</small>
                 </li>
             </ul>
         </nav>
